63aeb77d69683b8a105c476c431f4dc97a3e3477
catalamarti <marticatalasabate@gmail.com>
2024-08-12 08:05:48 -0700

    table

--- R/filterData.R
+++ R/filterData.R
@@ -5,11 +5,23 @@
 #' @param result A summarised_result object.
 #' @param resultType A resultType of interest to filter by.
 #' @param input Input of the shiny to filter by.
+#' @param prefixSet Prefix for setting columns.
+#' @param prefixGroup Prefix for groupping columns.
+#' @param showSettings Whether to show settings columns.
+#' @param showGroupping Whether to show groupping columns.
+#' @param pivotEstimates Whether to pivot estimates.
 #'
 #' @return The filtered result.
 #' @export
 #'
-filterData <- function(result, resultType, input) {
+filterData <- function(result,
+                       resultType,
+                       input,
+                       prefixSet = "",
+                       prefixGroup = "",
+                       showSettings = TRUE,
+                       showGroupping = TRUE,
+                       pivotEstimates = FALSE) {
   x <- result[[resultType]]
   inputs <- names(input)
 
@@ -24,8 +36,28 @@ filterData <- function(result, resultType, input) {
         dplyr::filter(.data[[nm]] %in% input[[fil]])
     }
   }
-  x <- x |>
-    dplyr::filter(.data$result_id %in% set$result_id)
+
+  if (isTRUE(showSettings)) {
+    set <- set |>
+      dplyr::rename_with(~ paste0(prefixSet, .x), !"result_id")
+    colsSet <- set |>
+      dplyr::mutate(dplyr::across(dplyr::everything(), as.character)) |>
+      tidyr::pivot_longer(!"result_id") |>
+      dplyr::filter(!is.na(.data$value)) |>
+      dplyr::pull("name") |>
+      unique()
+    x <- x |>
+      dplyr::inner_join(
+        set |>
+          dplyr::select(dplyr::all_of(c("result_id", colsSet))),
+        by = "result_id"
+      ) |>
+      dplyr::relocate(dplyr::all_of(colsSet), .after = "result_id") |>
+      dplyr::select(-"result_id")
+  } else {
+    x <- x |>
+      dplyr::filter(.data$result_id %in% set$result_id)
+  }
 
   # filter groupping
   group <- attr(x, "groupping")
@@ -38,8 +70,18 @@ filterData <- function(result, resultType, input) {
         dplyr::filter(.data[[nm]] %in% input[[fil]])
     }
   }
-  x <- x |>
-    dplyr::filter(.data$group_id %in% group$group_id)
+  if (isTRUE(showGroupping)) {
+    group <- group |>
+      dplyr::rename_with(~ paste0(prefixGroup, .x), !"group_id")
+    colsGroup <- colnames(group)[colnames(group) != "group_id"]
+    x <- x |>
+      dplyr::inner_join(group, by = "group_id") |>
+      dplyr::relocate(dplyr::all_of(colsGroup), .after = "group_id") |>
+      dplyr::select(-"group_id")
+  } else {
+    x <- x |>
+      dplyr::filter(.data$group_id %in% group$group_id)
+  }
 
   # filter variables and estimates
   varPrefix <- paste0(resultType, "_variables_and_estimates_")
@@ -50,6 +92,11 @@ filterData <- function(result, resultType, input) {
       x <- x |>
         dplyr::filter(.data[[nm]] %in% input[[fil]])
     }
+  }
+
+  if (isTRUE(pivotEstimates)) {
+    x <- x |>
+      pivotEstimatesTemp()
   }
 
   return(x)
@@ -76,7 +123,7 @@ prepareData <- function(result) {
     sety <- set |>
       dplyr::filter(.data$result_type == .env$rt)
     y <- result |>
-      dplyr::filter(.data$result_id %in% set$result_id) |>
+      dplyr::filter(.data$result_id %in% sety$result_id) |>
       dplyr::as_tibble()
     groupy <- y |>
       dplyr::select(dplyr::all_of(colsGroup)) |>
@@ -84,7 +131,10 @@ prepareData <- function(result) {
       dplyr::mutate("group_id" = dplyr::row_number()) |>
       dplyr::relocate("group_id")
     y <- y |>
-      dplyr::inner_join(groupy, by = colsGroup)
+      dplyr::inner_join(groupy, by = colsGroup) |>
+      dplyr::select(!dplyr::all_of(colsGroup)) |>
+      dplyr::relocate("result_id", "group_id") |>
+      dplyr::arrange(.data$result_id, .data$group_id)
     groupy <- groupy |>
       visOmopResults::splitAll()
     attr(y, "settings") <- sety
@@ -92,4 +142,38 @@ prepareData <- function(result) {
     x[[rt]] <- y
   }
   return(x)
+}
+
+pivotEstimatesTemp <- function(result) {
+  nameStyle <- "{estimate_name}"
+  types <- result |>
+    dplyr::select("estimate_type", "estimate_name") |>
+    dplyr::distinct() |>
+    dplyr::mutate("estimate_type" = dplyr::case_when(
+      grepl("percentage|proportion", .data$estimate_type) ~ "numeric",
+      "date" == .data$estimate_type ~ "Date",
+      .default = .data$estimate_type
+    )) |>
+    dplyr::distinct() |>
+    dplyr::group_by(.data$estimate_name) |>
+    dplyr::mutate("n" = dplyr::n()) |>
+    dplyr::ungroup()
+  notFormatted <- types |>
+    dplyr::filter(.data$n > 1) |>
+    dplyr::select("estimate_name") |>
+    dplyr::distinct() |>
+    dplyr::pull("estimate_name")
+  if (length(notFormatted) > 0) {
+    cli::cli_inform("The following estimates appear as character as multiple types are displayed: {notFormatted}.")
+  }
+  fq <- types |>
+    dplyr::filter(.data$n == 1)
+  fq <- paste0("as.", fq$estimate_type, "(.data$", fq$estimate_name, ")") |>
+    rlang::parse_exprs() |>
+    rlang::set_names(fq$estimate_name)
+  result |>
+    dplyr::select(-"estimate_type") |>
+    tidyr::pivot_wider(
+      names_from = "estimate_name", values_from = "estimate_value") |>
+    dplyr::mutate(!!!fq)
 }
