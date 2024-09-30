4aafb730999b449f798315b8aa10e931af160ef8
Núria Mercadé-Besora <61558739+nmercadeb@users.noreply.github.com>
2024-09-30 12:33:45 +0100

    checks

--- R/utilities.R
+++ R/utilities.R
@@ -1,6 +1,6 @@
 validateSummary <- function(summary, result){
   if (summary) {
-    sum <- capture.output(summary(result), type = "message")
+    sum <- utils::capture.output(summary(result), type = "message")
   } else {
     sum <- NULL
   }
