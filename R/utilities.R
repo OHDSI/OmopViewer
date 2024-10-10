f9621d118bedd9fb4c013174a4f6daf19264a89d
catalamarti <marticatalasabate@gmail.com>
2024-10-10 19:27:29 +0100

    add cardSummary

--- R/utilities.R
+++ R/utilities.R
@@ -1,11 +1,3 @@
-validateSummary <- function(summary, result){
-  if (summary) {
-    sum <- utils::capture.output(summary(result), type = "message")
-  } else {
-    sum <- NULL
-  }
-  return(sum)
-}
 
 validateDirectory <- function(directory) {
   # create directory if it does not exit
