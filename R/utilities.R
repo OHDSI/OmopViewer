28337fc1666aa01d9254ff1afe4c7e467279b7b9
catalamarti <marticatalasabate@gmail.com>
2024-11-10 11:50:50 +0000

    document

--- R/utilities.R
+++ R/utilities.R
@@ -31,69 +31,6 @@ validateDirectory <- function(directory) {
   }
   return(directory)
 }
-validatePanels <- function(panels, resultTypes, call = parent.frame()) {
-  omopgenerics::assertList(panels, call = call)
-
-  # elements must be characters
-  if (length(panels) > 0 & !is.character(unlist(panels))) {
-    cli::cli_abort("{.var panels} should be a character list.", call = call)
-  }
-
-  # elements must be unique TO CONSIDER REMOVING
-  allPanels <- purrr::flatten_chr(panels)
-  uniquePanels <- unique(allPanels)
-  if (length(allPanels) != length(uniquePanels)) {
-    cli::cli_abort("{.var panels} can not have repeated elements.", call = call)
-  }
-
-  # eliminate not present panels
-  panels <- panels |>
-    purrr::map(\(x) purrr::keep(x, \(y) y %in% resultTypes)) |>
-    purrr::compact()
-  notPresent <- allPanels[!allPanels %in% resultTypes]
-  if (length(notPresent) > 0) {
-    cli::cli_warn("{.var {notPresent}} eliminated from panels as not present in results.")
-  }
-
-  # add panels not in choices
-  panels <- c(panels, as.list(resultTypes[!resultTypes %in% allPanels]))
-
-  # correct lengths
-  panels <- panels |>
-    purrr::map(\(x) if (length(x) > 1) as.list(x) else x)
-
-  # correct names of panels and title
-  panels <- panelNames(panels)
-
-  return(panels)
-}
-panelNames <- function(x) {
-  # correct names of menus
-  if (is.null(names(x))) names(x) <- rep("", length(x))
-  names(x) <- names(x) |>
-    purrr::imap_chr(\(nm, k) {
-      if (nm != "") return(nm)
-      if (length(x[[k]]) == 1) { # if single panel it is not a menu
-        return(getTitle(x[[k]]))
-      } else {
-        return("unnamed")
-      }
-    })
-  # correct names of panels if not provided the name is the default one
-  x <- x |>
-    purrr::map(\(xx) {
-      if (is.list(xx)) {
-        if (is.null(names(xx))) names(xx) <- rep("", length(xx))
-        names(xx) <- names(xx) |>
-          purrr::imap_chr(\(nm, k) if (nm != "") nm else getTitle(xx[[k]]))
-      }
-      return(xx)
-    })
-  return(x)
-}
-getTitle <- function(x) {
-  getInfo(x, "title", formatTit(x))
-}
 validateBackground <- function(background, logo, call = parent.frame()) {
   msg <- "'background' must be either TRUE/FALSE or a path to an existing `.md` file."
   if (is.logical(background)) {
