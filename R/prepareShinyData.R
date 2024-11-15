175e88f0e9474d54f26caf8eab63bb9f56243673
catalamarti <marticatalasabate@gmail.com>
2024-11-15 16:54:55 +0000

    eliminate more functions

--- R/prepareShinyData.R
+++ R/prepareShinyData.R
@@ -1,29 +1,2 @@
 
-#' Prepare a list of summarised_result objects.
-#'
-#' @param result A summarised_result object
-#' @param panelDetails A list with the panel details.
-#'
-#' @return A list of summarised_result objects.
-#' @export
-#'
-prepareShinyData <- function(result,
-                             panelDetails = NULL) {
-  # check inputs
-  result <- omopgenerics::validateResultArgument(result)
-  omopgenerics::assertList(panelDetails, null = TRUE)
-  panelDetails <- validatePanelDetails(panelDetails, result)
 
-  prepareResult(panelDetails = panelDetails, result = result)
-}
-prepareResult <- function(panelDetails, result) {
-  panelDetails |>
-    purrr::map(\(x) {
-      result |>
-        dplyr::filter(.data$result_id %in% .env$x$result_id) |>
-        omopgenerics::newSummarisedResult(
-          settings = omopgenerics::settings(result) |>
-            dplyr::filter(.data$result_id %in% .env$x$result_id)
-        )
-    })
-}
