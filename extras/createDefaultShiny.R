65be83573ed379d27a8be90e2d24b461b7ae4ac8
catalamarti <marticatalasabate@gmail.com>
2025-04-11 14:30:47 +0100

    Update createDefaultShiny.R

--- extras/createDefaultShiny.R
+++ extras/createDefaultShiny.R
@@ -1,5 +1,7 @@
 
-rlang::check_installed("devtools")
+if (!rlang::is_installed("devtools")) {
+  install.packages("devtools")
+}
 devtools::load_all()
 
 exportStaticApp(result = omopViewerResults, directory = here::here("extras"))
