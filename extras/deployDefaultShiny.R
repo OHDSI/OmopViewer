6de8058a3ca4c12bfcd5bb1454d9038e2616378e
catalamarti <marticatalasabate@gmail.com>
2025-04-11 15:06:46 +0100

    overwrite

--- extras/deployDefaultShiny.R
+++ extras/deployDefaultShiny.R
@@ -4,4 +4,4 @@ if (!rlang::is_installed("rsconnect")) {
 rsconnect::setAccountInfo(name = "dpa-pde-oxford",
                           token = Sys.getenv("SHINYAPPS_TOKEN"),
                           secret = Sys.getenv("SHINYAPPS_SECRET"))
-rsconnect::deployApp(appDir = "extras/shiny", appName = "OmopViewerExample")
+rsconnect::deployApp(appDir = "extras/shiny", appName = "OmopViewerExample", forceUpdate = TRUE)
